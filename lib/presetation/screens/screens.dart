c5208d58012e1a479623db74e2ab2a738f3d49b6
Deijai Miranda Almeida <djairn18@gmail.com>
2023-11-25 18:17:55 -0300

    Screens e exports aatualizadas

--- lib/presetation/screens/screens.dart
+++ lib/presetation/screens/screens.dart
@@ -1,3 +1,9 @@
+export 'package:widgets_app/presetation/screens/animated/animated_screen.dart';
+export 'package:widgets_app/presetation/screens/app_tutorial/app_tutorial_screen.dart';
+export 'package:widgets_app/presetation/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widgets_app/presetation/screens/progress/progreess_screen.dart';
+export 'package:widgets_app/presetation/screens/snackbar/snackbar_screen.dart';
+export 'package:widgets_app/presetation/screens/ui_controls/ui_controls_screen.dart';
 export 'package:widgets_app/presetation/screens/buttons/buttons_screen.dart';
 export 'package:widgets_app/presetation/screens/cards/cards_screen.dart';
 export 'package:widgets_app/presetation/screens/home/home_screen.dart';
