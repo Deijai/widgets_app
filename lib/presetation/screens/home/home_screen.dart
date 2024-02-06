cf0fc2eca3d521cb9e31c6793a25cfadf47a57ea
Deijai Miranda Almeida <djairn18@gmail.com>
2024-02-06 10:24:05 -0300

    Ui screen finished - parte 2

--- lib/presetation/screens/home/home_screen.dart
+++ lib/presetation/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presetation/widgets/drawer/side_menu.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String name = 'home_screen';
@@ -8,11 +9,17 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final scaffoldkey = GlobalKey<ScaffoldState>();
+
     return Scaffold(
+      key: scaffoldkey,
       appBar: AppBar(
         title: const Text('Flutter + Material 3'),
       ),
       body: const _HomeView(),
+      drawer: SideMenu(
+        scaffoldKey: scaffoldkey,
+      ),
     );
   }
 }
